51e9a224db14ac7a8c3326a63a020e6c6e419a91
Fabrice Servais <fabrice.servais@gmail.com>
2014-04-23 22:48:13 +0200

    First buggy implementation of Laplace smoothing

--- estimateQFromRealization.m
+++ estimateQFromRealization.m
@@ -11,7 +11,19 @@ function [ Q ] = estimateQFromRealization( tr )
     for i=1:numOfRealizations-1
         Q(tr(i),tr(i+1)) = Q(tr(i),tr(i+1))+1;
     end
-    Q = Adj2Q(Q); % It's not an adjacency matrix, but the computation is the same...
-
+    
+    % It's not an adjacency matrix, but the computation is the same...
+    %Q = Adj2Q(Q); 
+    
+    % Other solution with Laplace estimation... -> Needs debug
+    s = sum(Q,2)
+    nodes = 50;
+    for i=1:size(Q,1) % Row
+        for j=1:size(Q,2) % Col
+            Q(i,j) = (Q(i,j)+1)/(s(i)+nodes); % Hell, the sum over a line doesn't equal to 1...
+        end
+    end
+    
+    
 end
 
