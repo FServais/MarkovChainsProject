b49174583e6515786649ac149784673a99a61be1
Fabrice Servais <fabrice.servais@gmail.com>
2014-04-24 09:00:52 +0200

    Working Laplace smoothing

--- estimateQFromRealization.m
+++ estimateQFromRealization.m
@@ -5,7 +5,9 @@
 %
 % @return Q Transition matrix
 function [ Q ] = estimateQFromRealization( tr )
+
     % Transition : from i to j
+    % Fill the matrix with the number of occurences
     numOfRealizations = length(tr);
     Q = zeros(numOfRealizations,numOfRealizations);
     for i=1:numOfRealizations-1
@@ -13,14 +15,13 @@ function [ Q ] = estimateQFromRealization( tr )
     end
     
     % It's not an adjacency matrix, but the computation is the same...
-    %Q = Adj2Q(Q); 
+    % Q = Adj2Q(Q); 
     
-    % Other solution with Laplace estimation... -> Needs debug
-    s = sum(Q,2)
+    % Other solution with Laplace estimation
     nodes = 50;
     for i=1:size(Q,1) % Row
         for j=1:size(Q,2) % Col
-            Q(i,j) = (Q(i,j)+1)/(s(i)+nodes); % Hell, the sum over a line doesn't equal to 1...
+            Q(i,j) = (Q(i,j)+1)/(numOfRealizations+nodes); 
         end
     end
     
