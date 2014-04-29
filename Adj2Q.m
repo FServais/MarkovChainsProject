fd2a2913a78a78f31e366574d7c16bd1053d0588
Waliens <romainmormont@hotmail.com>
2014-04-29 21:32:55 +0200

    change function documentation

--- Adj2Q.m
+++ Adj2Q.m
@@ -1,11 +1,8 @@
 function Q = Adj2Q(A)
-% Returns the transition matrix Q (with uniform probability) for an
-% adjacency matrix A
-% @param A the adjacency matrix (containing no dangdling nodes
-% @return Q the transition matrix
-    sumRows = arrayfun(@replaceZero,sum(A, 2));
-    
-    
+%Adj2Q compute the transition matrix for an adjacency matrix
+%   [Q] = Adj2Q(A) : if A is an adjacency matrix, Q is the corresponding
+%   transition matrix
+    sumRows = arrayfun(@replaceZero,sum(A, 2))
     sumMat = repmat(sumRows, 1, size(A, 2));
     Q = A ./ sumMat;
 end
--- findStationnaryPi.m
+++ findStationnaryPi.m
@@ -1,19 +1,24 @@
 function [pi_s, i] = findStationnaryPi(Q, pi_0, tol, max_iter)
-% Returns the stationnary vector of probability for each state
-% of the graph
-%
-% @param Q the transition matrix
-% @param pi_0 the intial probability distribution
-% @param tol (optional) the max variation of one probability from one step to another
-% @param max_iter (optional) the max number of iteration 
-%
-% @return pi the vector containing the stationnary distribution
+%findStationnaryPi compute the stationnary distribution for a transition
+%matrix starting with a given ditribution
+%   [pi_s, i] = findStationnaryPi(Q, pi_0) : if Q is a transition matrix
+%   and pi_0 the starting distribution, then pi_s is the sationnary
+%   distribution and i is the number of iteration to reach this
+%   distribution
+%   [pi_s, i] = findStationnaryPi(Q, pi_0, tol) : performs the same
+%   computation except that the maximum error between two consecuting
+%   distribution is less than tol (default value for tol : 10^-10)
+%   [pi_s, i] = findStationnaryPi(Q, pi_0, tol, max_iter) : performs the
+%   same computation except that the computations stops when max_iter is
+%   reached (default value for max_iter : 10000)
 
 	if (nargin == 2)
 		max_iter = 10000;
 		tol = 10^-10;
 	elseif (nargin == 3)
 		max_iter = 10000;
+    elseif (nargin ~= 4)
+        error('Invalid number of arguments');
 	end
 
     pi_prev = pi_0;
