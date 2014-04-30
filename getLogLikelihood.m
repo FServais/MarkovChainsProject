5cf273c6e3f053c1e3e3e43c1b16424c07f9de4c
Waliens <romainmormont@hotmail.com>
2014-04-30 18:24:18 +0200

    New implementation for getLogLikelihood

--- getLogLikelihood.m
+++ getLogLikelihood.m
@@ -5,19 +5,19 @@ function p = getLogLikelihood(Q,X)
 %   and X an array of which the lines are markov chains (of the same size),
 %   p is a matrix of which the element at row i and column j is the 
 %   log-likelihood of the ith transition matrix for the jth markov chain
-    
     [x_q, y_q, z_q] = size(Q);
-    [x_c, y_c] = size(X);
+    [x_c, ~] = size(X);
     
     if y_q ~= z_q
         error('Second and third dimensions of Q must be the same : a transition matrix is a square matrix');
     end
     
     p = zeros(x_q, x_c);
-    
+
     for i = 1:x_c
-        for j = 1:(y_c - 1)
-            p(:,i) = p(:,i) + log(Q(:, X(i,j), X(i,j + 1)));
+        Tr = countTransitions(X(i,:), y_q); 
+        for j = 1:x_q
+            p(j,i) = sum(sum(sum(log(squeeze(Q(j,:,:))) .* Tr)));
         end
     end
 end
