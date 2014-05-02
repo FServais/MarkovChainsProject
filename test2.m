54a3e0b12e263207f9ede1468dc61da5d204ccc7
Waliens <romainmormont@hotmail.com>
2014-05-02 15:52:48 +0200

    NEw function + minor modif to report

--- test2.m
+++ test2.m
@@ -1,61 +1,49 @@
-k = 7; % graph order
 
-A1 = [0 1 1 0 0 1 1;
-      0 0 0 1 0 0 0;
-      1 1 0 0 0 0 0;
-      0 0 0 0 1 0 0;
-      1 0 0 0 0 0 1;
-      0 1 0 1 0 0 0;
-      0 0 0 0 1 1 0];
+
+ file_c = load('test2_adjacency.mat');
  
-A2 = [0 0 0 0 0 0 1;
-      0 0 0 1 1 0 0;
-      0 1 0 0 0 0 0;
-      0 0 1 0 1 0 0;
-      1 0 1 0 0 0 1;
-      0 1 0 1 1 0 0;
-      0 1 1 0 0 1 0];
-
+ G = file_c.G;
+    
+ [k, ~] = size(G);
  % Compute 
- Q1 = getQTeleport(A1, 0.15);
- Q2 = getQTeleport(A2, 0.15);
- pi_0 = ones(1,k) / k;
+ Q1 = getRandomQTeleport(G, 0.50); % surfer 1
+ Q2 = getRandomQTeleport(G, 0.50); % surfer 2
+ pi_0 = zeros(k, 1);
+ pi_0(1) = 1;
  
- n = 1; % number of traces
- m = 140; % trace size
+ n = 200; % number of traces
+ m = 100; % trace size
  
+ %% Estimations of Q
  Tr_est1 = GenMarkov(Q1, pi_0, m).';
  Tr_est2 = GenMarkov(Q2, pi_0, m).';
-
-%  a = countTransitions(Tr_est1, k);
-%  b = countTransitions(Tr_est2, k);
-%  
-%  a = a + ones(k,k)
-%  b = b + ones(k,k)
-% 
-%  Q_est1 = a ./ repmat(sum(a, 2), 1, k)
-%  Q_est2 = b ./ repmat(sum(b, 2), 1, k)
  
  Q_est1 = estimateQ(Tr_est1,k);
  Q_est2 = estimateQ(Tr_est2,k);
  
- Tr1 = GenMarkov(Q1, pi_0, m).';
- Tr2 = GenMarkov(Q1, pi_0, m).';
- Tr3 = GenMarkov(Q2, pi_0, m).';
- Tr4 = GenMarkov(Q2, pi_0, m).';
- Tr5 = GenMarkov(Q1, pi_0, m).';
- Tr6 = GenMarkov(Q1, pi_0, m).';
- Tr7 = GenMarkov(Q2, pi_0, m).';
- Tr8 = GenMarkov(Q2, pi_0, m).';
+ %% Trace that must be evaluated
+ Trmat = zeros(n, m);
  
- Tr = [Tr1; Tr2; Tr3; Tr4; Tr5; Tr6; Tr7; Tr8];
+ count_Q1 = 0;
+ for i = 1:n
+    if i <= (n / 2)
+        count_Q1 = count_Q1 + 1;
+        Trmat(i,:) = GenMarkov(Q1, pi_0, m).';
+    else
+        Trmat(i,:) = GenMarkov(Q2, pi_0, m).';
+    end
+ end
  
  Q = zeros(2,k,k);
  Q(1,:,:) = Q_est1;
  Q(2,:,:) = Q_est2;
  
- p = getLogLikelihood(Q, Tr);
+ %% Computation of likelihood and origin
+ p = getLogLikelihood(Q, Trmat);
  
- getOrigin(Q,Tr,[0.5 0.5])
+ P_Q1 = count_Q1/n;
+ P_Q2 = 1 - P_Q1;
+ 
+ o = getOrigin(Q,Trmat,[P_Q1 P_Q2]);
  
  
