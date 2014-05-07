1d2ea8e8541c81135e8dd0b7b97226c5c7fe17b9
Fabrice Servais <fabrice.servais@gmail.com>
2014-05-07 19:44:27 +0200

    Orthographe

--- estimateQ.m
+++ estimateQ.m
@@ -1,6 +1,6 @@
 function Q = estimateQ(X, k)
 %ESTIMATEQ estimate the transition matrix of a markov chain
-%   Q = ESTIMATEQ(X,k) : if X is a markov chaine and k the number of 
+%   Q = ESTIMATEQ(X,k) : if X is a markov chain and k the number of 
 %   possible state, then Q is the transition matrix estimated using the 
 %   maximum likelihood method
 
